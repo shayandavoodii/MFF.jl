bb3c52714104d1eb2187fba8640d7b57da4ca37d
Shayan <sh0davoodi@gmail.com>
2025-01-29 19:11:14 +0330

    [src/utils.jl] `check_prprty` expired 🧹

--- src/utils.jl
+++ src/utils.jl
@@ -32,10 +32,3 @@ Saved dates data to <PATH>
 ```
 """
 function gs end
-
-function check_prprty(val, prprty)
-  isequal(val, nothing) && error(
-    """property ($prprty) is not valid or doesn't exist. Valid properties are:
-    \"timestamp", \"open", \"high", \"low", \"close", \"adjclose", \"vol". """
-  )
-end
